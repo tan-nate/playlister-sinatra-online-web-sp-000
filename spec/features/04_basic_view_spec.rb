4e228d3b7c703c6d729d873f8c6829830367cce3
Nate Tan <nktan93@gmail.com>
2019-06-10 18:52:36 +0000

    Automatically backed up by Learn

--- spec/features/04_basic_view_spec.rb
+++ spec/features/04_basic_view_spec.rb
@@ -99,7 +99,7 @@ describe "Playlister Basics" do
       end
     end
 
-    describe "/artists/:slug" do
+    describe "/artists/:slug" do 
       before do
         visit "/artists/#{@artist.slug}"
       end
