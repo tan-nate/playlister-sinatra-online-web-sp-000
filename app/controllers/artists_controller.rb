31c3044b8f6df62151aa7fe4a50078d1f803fdb2
Nate Tan <nktan93@gmail.com>
2019-06-10 19:04:35 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,8 +6,8 @@ class ArtistsController < ApplicationController
 
   get '/artists/:slug' do
     @artist = Artist.all.find_by_slug(params[:slug])
-    @songs = Song.all
-    @genres = Genre.all
+    @songs = @artist.songs
+    @genres = @artist.genres
     erb :'artists/show'
   end
 end
